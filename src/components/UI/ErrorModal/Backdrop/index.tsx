b81bb696ec704a532b71d2c70384644ae0e0a244
Burak CAN <drcan94@gmail.com>
2023-07-25 12:13:54 +0300

    Error Modal error is fixed

--- src/components/UI/ErrorModal/Backdrop/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StyledBackdrop } from "./styles";
-
-type BackdropProps = {
-  onConfirm: () => void;
-};
-
-export const Backdrop: React.FC<BackdropProps> = ({ onConfirm }) => {
-  return <StyledBackdrop onClick={onConfirm} />;
-};
